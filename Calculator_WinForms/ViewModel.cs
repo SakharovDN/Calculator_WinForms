84b852bb9d8b103d26bf0ff9b0e110b1ca9705ff
SakharovDN <deafsah@gmail.com>
2022-11-25 21:25:12 +0700

    Project refactored

--- Calculator_WinForms/ViewModel.cs
+++ Calculator_WinForms/ViewModel.cs
@@ -5,150 +5,145 @@ using System.Runtime.CompilerServices;
 
 public class ViewModel : INotifyPropertyChanged
 {
-	#region Constants
+    #region Constants
 
-	public const string ZERO_STRING = "0";
-	public const string POINT_STRING = ",";
+    public const string ZERO_STRING = "0";
+    public const string POINT_STRING = ",";
 
-	#endregion
+    #endregion
 
-	#region Fields
+    #region Fields
 
-	private string _resultText;
-	private string _equationText;
-	private bool _isShowingResult;
+    private string _resultText;
+    private string _equationText;
+    private bool _isShowingResult;
 
-	#endregion
+    #endregion
 
-	#region Properties
+    #region Properties
 
-	public Equation Equation { get; private set; }
+    public Equation Equation { get; private set; }
 
-	public string ResultText
-	{
-		get => _resultText;
-		private set
-		{
-			_resultText = value;
-			OnPropertyChanged();
-		}
-	}
+    public string ResultText
+    {
+        get => _resultText;
+        private set
+        {
+            _resultText = value;
+            Equation.SetOperand(_resultText);
+            OnPropertyChanged();
+        }
+    }
 
-	public string EquationText
-	{
-		get => _equationText;
-		private set
-		{
-			_equationText = value;
-			OnPropertyChanged();
-		}
-	}
-	#endregion
+    public string EquationText
+    {
+        get => _equationText;
+        private set
+        {
+            _equationText = value;
+            OnPropertyChanged();
+        }
+    }
 
-	#region Events
+    #endregion
 
-	public event PropertyChangedEventHandler? PropertyChanged;
+    #region Events
 
-	#endregion
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-	#region Constructors
+    #endregion
 
-	public ViewModel()
-	{
-		_equationText = string.Empty;
-		_resultText = ZERO_STRING;
-		Equation = new Equation();
-	}
+    #region Constructors
 
-	#endregion
+    public ViewModel()
+    {
+        _equationText = string.Empty;
+        _resultText = ZERO_STRING;
+        Equation = new Equation();
+    }
 
-	#region Methods
+    #endregion
 
-	public void SetDigit(string digitString)
-	{
-		if (_isShowingResult)
-		{
-			Cancel();
-		}
+    #region Methods
 
-		if (!int.TryParse(digitString, out int _))
-		{
-			return;
-		}
+    public void OnDigitButtonPressed(string digitString)
+    {
+        if (_isShowingResult)
+        {
+            Cancel();
+        }
 
-		if (ResultText == ZERO_STRING)
-		{
-			ResultText = digitString;
-		}
-		else
-		{
-			ResultText += digitString;
-		}
+        if (!int.TryParse(digitString, out int _))
+        {
+            return;
+        }
 
-		Equation.SetOperand(ResultText);
-	}
+        if (ResultText == ZERO_STRING || Equation.Operators.Count == Equation.Operands.Count)
+        {
+            ResultText = digitString;
+        }
+        else
+        {
+            ResultText += digitString;
+        }
+    }
 
-	public void SetOperator(string operatorString)
-	{
-		Equation.SetOperator(operatorString);
-		ResultText = ZERO_STRING;
-		EquationText = Equation.GetEquationText();
-		_isShowingResult = false;
-	}
+    public void OnOperatorButtonPressed(string operatorString)
+    {
+        Equation.SetOperator(operatorString);
+        EquationText = Equation.GetEquationText();
+        _isShowingResult = false;
+    }
 
-	public void ShowResult()
-	{
-		if (!Equation.IsCalculatable)
-		{
-			return;
-		}
+    public void OnEqualButtonPressed()
+    {
+        if (!Equation.IsCalculatable)
+        {
+            return;
+        }
 
-		ResultText = Equation.GetResult().ToString();
-		EquationText = string.Empty;
-		_isShowingResult = true;
-	}
+        ResultText = Equation.GetResult().ToString();
+        EquationText = string.Empty;
+        _isShowingResult = true;
+    }
 
-	public void Cancel()
-	{
-		ResultText = ZERO_STRING;
-		Equation = new Equation();
-		EquationText = string.Empty;
-		_isShowingResult = false;
-	}
+    public void Cancel()
+    {
+        ResultText = ZERO_STRING;
+        Equation = new Equation();
+        EquationText = string.Empty;
+        _isShowingResult = false;
+    }
 
-	public void OnBackspaceClicked()
-	{
-		if (ResultText == ZERO_STRING || _isShowingResult)
-		{
-			return;
-		}
+    public void OnBackspaceButtonPressed()
+    {
+        if (ResultText == ZERO_STRING || _isShowingResult)
+        {
+            return;
+        }
 
-		ResultText = ResultText.Length == 1 ? ZERO_STRING : ResultText.Remove(ResultText.Length - 1);
+        ResultText = ResultText.Length == 1 ? ZERO_STRING : ResultText.Remove(ResultText.Length - 1);
+    }
 
-		Equation.SetOperand(ResultText);
-	}
+    public void SetPoint()
+    {
+        if (_isShowingResult)
+        {
+            Cancel();
+        }
 
-	public void SetPoint()
-	{
-		if (_isShowingResult)
-		{
-			Cancel();
-		}
+        if (ResultText.Contains(POINT_STRING))
+        {
+            return;
+        }
 
-		if (ResultText.Contains(POINT_STRING))
-		{
-			return;
-		}
+        ResultText += POINT_STRING;
+    }
 
-		ResultText += POINT_STRING;
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
-		Equation.SetOperand(ResultText);
-	}
-
-	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
-
-	#endregion
+    #endregion
 }
